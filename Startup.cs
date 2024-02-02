953128337d89d1fe7f71e6d37b8711fccfd2c1fd
Alexander Kharkevich <alexander_kharkevich@outlook.com>
2024-02-02 16:46:39 -0500

    feat: add health checks

--- Startup.cs
+++ Startup.cs
@@ -32,6 +32,7 @@ namespace infra_api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "infra_api", Version = "v1" });
             });
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +52,7 @@ namespace infra_api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
             });
         }
     }
